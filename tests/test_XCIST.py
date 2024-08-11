c8e6a6076edf99b232fee188afd31741a23fd28c
brandon.nelson <brandon.nelson@scl.fda.gov>
2024-08-10 21:14:55 -0400

    fixed filepath in tests

--- tests/test_XCIST.py
+++ tests/test_XCIST.py
@@ -1,27 +1,25 @@
-# %%
-import numpy as np
+from pathlib import Path
 from shutil import rmtree
+
+import numpy as np
 
 from pedsilicoICH.image_acquisition import read_dicom, CTobj
 
-dcm = '/gpfs_projects/brandon.nelson/RSTs/pediatricIQphantoms/CTP404/diameter_350mm/350mm_CTP404_groundtruth.dcm'
-phantom = read_dicom(dcm)
-phantom = np.repeat(phantom[None], 11, axis=0)
-                    
-# %%
-from pathlib import Path
 
 def get_effective_diameter(ground_truth_mu, pixel_width_mm):
     A = np.sum(ground_truth_mu>-1000)*pixel_width_mm**2
     return 2*np.sqrt(A/np.pi)
 
+base_dir = Path(__file__).parent
+dcm = base_dir / '350mm_CTP404_groundtruth.dcm'
+phantom = read_dicom(dcm)
+phantom = np.repeat(phantom[None], 11, axis=0)
 diameter_pixels = get_effective_diameter(phantom[0], 1)
 known_diameter_mm = 200
 fov_mm = phantom.shape[-1]*known_diameter_mm/diameter_pixels
-fov_mm
 dx = fov_mm/phantom.shape[-1]
 
-test_dir = Path(__file__).parent / 'test_result'
+test_dir = base_dir / 'test_result'
 if Path(test_dir).exists():
     rmtree(test_dir)
 ct = CTobj(phantom, spacings=3*[dx], patientname='test', output_dir=test_dir)
@@ -37,5 +35,3 @@ def test_scan():
     assert(ct.projections.shape==(views, ct.xcist.cfg.scanner.detectorRowCount, ct.xcist.cfg.scanner.detectorColCount))
     assert(len(dcms) == len(ct.start_positions))
     assert(dcms_in_dir == dcms)
-
-# %%
