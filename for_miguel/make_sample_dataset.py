5f8b160c0e451fa3032b7d85f700a8f057df22bf
brandon.nelson <brandon.nelson@scl.fda.gov>
2024-12-06 18:50:16 -0500

    important fix to scanner for when MIDA doesnt add a lesion

--- for_miguel/make_sample_dataset.py
+++ for_miguel/make_sample_dataset.py
@@ -67,7 +67,7 @@ while len(lesions) < max_lesions:
     lesion = dict()
     lesion_type = rng.choice(lesion_types, size=1)[0]
     if lesion_type == 'round':
-        lesion['volume'] = rng.choice(np.linspace(0.1, 8),
+        lesion['volume'] = rng.choice(np.linspace(0.1, 7.6),
                                       size=1)[0]
     else:
         lesion['volume'] = rng.choice(np.linspace(1, 20),
@@ -83,7 +83,7 @@ scans = []
 
 kVps = [80, 90, 100, 110, 120, 130, 140]
 mAs = list(range(10, 1510, 10))
-views = 100
+views = 1000
 
 ref_mA = 200
 ref_kVp = 120
@@ -109,9 +109,9 @@ results = dict(uid=[], age=[], lesion_type=[], volume=[], attenuation=[],
 desired_iph = desired_control = desired_edh = desired_sdh = 25
 n_control = n_iph = n_sdh = n_edh = 0
 
+desired_counts_reached = False
 uid = 0
-while (n_control < desired_control) & (n_iph < desired_iph) &\
-      (n_edh < desired_edh) & (n_sdh < desired_sdh):
+while not desired_counts_reached:
     print(f'{n_control}/{desired_control} control\n{n_iph}/{desired_iph} IPH\n\
 {n_sdh}/{desired_sdh} SDH\n{n_edh}/{desired_edh} EDH')
     lesion = rng.choice(lesions, size=1)[0]
@@ -119,57 +119,61 @@ while (n_control < desired_control) & (n_iph < desired_iph) &\
     recon = rng.choice(recons, size=1)[0]
     age = float(rng.choice(possible_ages, size=1)[0])
     phantom = load_phantom(age)
-    scan_seed = rng.uniform(0, 1e6, size=1)[0]
+    scan_seed = rng.integers(0, 1e6, size=1)[0]
     fname = outdir / f"{uid:03d}_{age}_yrs_{lesion['lesion_type']}_{scan['kVp']}kV_{scan['mA']}mA.png"
-# try:
-    if lesion['lesion_type']:
-        phantom.insert_lesion(**lesion, seed=scan_seed)
-    scanner = Scanner(phantom)
-    if lesion['lesion_type'] is None:
-        center = rng.choice(scanner.calculate_start_positions()[1:-1],
-                            size=1)[0]
-    else:
-        center = (phantom.get_CT_number_phantom().shape[0]/2 -
-                  phantom._lesion_coords[0][0])*phantom.dz
-    n_scans = 1
-    scan_width = 7
-    width = scan_width*n_scans
-    scan['startZ'] = center - width / 2
-    scan['endZ'] = center + width / 2
+    try:
+        if lesion['lesion_type']:
+            phantom.insert_lesion(**lesion, seed=scan_seed)
+        scanner = Scanner(phantom)
+        if lesion['lesion_type'] is None:
+            center = rng.choice(scanner.calculate_start_positions()[1:-1],
+                                size=1)[0]
+        else:
+            center = (phantom.get_CT_number_phantom().shape[0]/2 -
+                      phantom._lesion_coords[0][0])*phantom.dz
+        n_scans = 1
+        width = round(scanner.scan_width, ndigits=1)*n_scans
+        scan['startZ'] = center - width / 2
+        scan['endZ'] = center + width / 2
 
-    scanner.run_scan(**scan)
-    scanner.run_recon(**recon)
-    if recon['sliceThickness'] == 5:
-        scanner.recon = scanner.recon.mean(axis=0)
-    ctshow(scanner.recon, 'brain')
-    plt.savefig(fname, dpi=600, bbox_inches='tight')
-    # update metadata
-    results['uid'].append(uid)
-    results['age'].append(age)
-    if lesion['lesion_type'] == 'round':
-        results['lesion_type'].append('intraparenchymal')
-    else:
-        results['lesion_type'].append(lesion['lesion_type'])
-    results['volume'].append(np.round(lesion['volume'], decimals=1))
-    results['attenuation'].append(np.round(lesion['intensity'], decimals=1))
-    results['kVp'].append(scan['kVp'])
-    results['mA'].append(scan['mA'])
-    results['kernel'].append(recon['kernel'])
-    results['slice_thickness'].append(recon['sliceThickness'])
-    results['file'] = fname.relative_to(outdir.parent)
-    results['seed'].append(scan_seed)
-    pd.DataFrame(results).to_csv(outdir.parent / 'metadata.csv',
-                                 index=False)
-    if lesion['lesion_type'] == 'round':
-        n_iph += 1
-    elif lesion['lesion_type'] == 'epidural':
-        n_edh += 1
-    elif lesion['lesion_type'] == 'subdural':
-        n_sdh += 1
-    elif lesion['lesion_type'] is None:
-        n_control += 1
-    uid += 1
-    # except Exception as err:
-    #     logger.error(f"Runtime error occurred at {fname}\n{err}")
-    #     continue
+        scanner.run_scan(**scan)
+        scanner.run_recon(**recon)
+        if recon['sliceThickness'] == 5:
+            scanner.recon = scanner.recon.mean(axis=0)
+        ctshow(scanner.recon, 'brain')
+        plt.savefig(fname, dpi=600, bbox_inches='tight')
+        # update metadata
+        results['uid'].append(uid)
+        results['age'].append(age)
+        if lesion['lesion_type'] == 'round':
+            results['lesion_type'].append('intraparenchymal')
+        else:
+            results['lesion_type'].append(lesion['lesion_type'])
+        results['volume'].append(np.round(lesion['volume'], decimals=1))
+        results['attenuation'].append(np.round(lesion['intensity'],
+                                               decimals=1))
+        results['kVp'].append(scan['kVp'])
+        results['mA'].append(scan['mA'])
+        results['kernel'].append(recon['kernel'])
+        results['slice_thickness'].append(recon['sliceThickness'])
+        results['file'] = fname.relative_to(outdir.parent)
+        results['seed'].append(scan_seed)
+        pd.DataFrame(results).to_csv(outdir.parent / 'metadata.csv',
+                                     index=False)
+        if lesion['lesion_type'] == 'round':
+            n_iph += 1
+        elif lesion['lesion_type'] == 'epidural':
+            n_edh += 1
+        elif lesion['lesion_type'] == 'subdural':
+            n_sdh += 1
+        elif lesion['lesion_type'] is None:
+            n_control += 1
+        uid += 1
+        if (n_control >= desired_control) & (n_iph >= desired_iph) &\
+           (n_edh >= desired_edh) & (n_sdh >= desired_sdh):
+            desired_counts_reached = True
+    except Exception as err:
+        logger.error(f"Runtime error occurred at {fname}\nlesion: {lesion}\n\
+scan: {scan}\n recon: {recon}\nseed: {scan_seed}\n{err}")
+        continue
 # %%
