d1411d51f74d04eed8a1bc59a3e2170626b4ff4c
Brandon J Nelson <brandon.nelson@fda.hhs.gov>
2024-12-17 14:29:32 -0500

    small fixes, batchmode configs work for me on luna

--- run_batchmode.sh
+++ run_batchmode.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-CONFIG=${1: example_config.toml}
+CONFIG=${1:-example_config.toml}
 SIM_NAME=pedsilicoICH_$(date +'%m-%d-%Y_%H-%M')
 LOG_DIR=logs/$SIM_NAME
-SAVE_DIR=/projects01/didsr-aiml/brandon.nelson/pedsilicoICH/$SIM_NAME
+SAVE_DIR=/projects01/didsr-aiml/$USER/pedsilicoICH/$SIM_NAME
 
 desired_cases_line=$(grep "desired_cases=" "$CONFIG")
 COUNT=$(echo "$desired_cases_line" | sed 's/desired_cases=\([0-9]*\).*/\1/')
@@ -11,4 +11,4 @@ echo Running $COUNT simulation conditions
 
 START_TASK=1
 END_TASK=$COUNT
-qsub -N $SIM_NAME -t $START_TASK-$END_TASK batchmode_CT_dataset_pipeline.sge $LOG_DIR $SAVE_DIR $CONFIG
+qsub -N $SIM_NAME -t $START_TASK-$END_TASK batchmode_CT_dataset_pipeline.sge $LOG_DIR $SAVE_DIR $COUNT $CONFIG
