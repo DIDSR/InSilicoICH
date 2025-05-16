0fd2fc7956dcc1501331e832e77807b26d1d443d
brandon.nelson <brandon.nelson@scl.fda.gov>
2025-05-16 19:50:39 -0400

    removed unnecesarry print message

--- src/insilicoICH/study.py
+++ src/insilicoICH/study.py
@@ -25,7 +25,6 @@ def get_phantoms_dict():
     pm = pluggy.PluginManager(hooks.PROJECT_NAME)
     pm.add_hookspecs(hooks.PhantomSpecs)
     num_loaded = pm.load_setuptools_entrypoints(group=hooks.PROJECT_NAME)
-    print(f"Loaded {num_loaded} plugins via entry points.")
 
     # --- Call the hook to get all registered phantom types ---
     # The hook returns a list of lists (one list per plugin implementation that returned something)
@@ -35,15 +34,6 @@ def get_phantoms_dict():
     for result_list in list_of_results:
         if result_list:  # Check if the plugin returned a non-empty list
             discovered_phantom_classes.update(result_list)
-
-    print("Discovered Phantom Types (Classes):")
-    for cls_name, cls in discovered_phantom_classes.items():
-        print(f"- {cls_name} - {cls}")
-
-    # Get just the names for your list
-    phantom_type_names = discovered_phantom_classes.keys()
-    print(f"\nDiscovered {len(phantom_type_names)} Phantom Names:")
-    print(phantom_type_names)
     return discovered_phantom_classes
 
 
