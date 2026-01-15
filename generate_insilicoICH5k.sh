4033e7433d3b5adf6a10f5d0361e6b5635df1c9b
Brandon J Nelson <brandon.nelson@fda.hhs.gov>
2026-01-15 18:46:17 -0500

    local commit

--- generate_insilicoICH5k.sh
+++ generate_insilicoICH5k.sh
@@ -1,7 +1,9 @@
-if [ $(generate --help | grep InSilicoLVO | wc -l) -eq 1 ]; then
-    generate /projects01/didsr-aiml/brandon.nelson/rsna-aneurysm-synthetic-detection/synthetic-aneurysm-50k/synthetic-aneurysm-50k_study_plan.csv --parallel --chunk_size 2000 #--overwrite
+#!/bin/sh
+#SBATCH --time=700:00:00        # time limit
+if [ $(generate --help | grep InSilicoICH | wc -l) -eq 1 ]; then
+    generate results/results_study_plan.csv --parallel --chunk_size 2000 #--overwrite
 else
-    echo "Error: Invalid version of generate function present. Please follow the steps below to reinstall InSilicoLVO:"
-    echo "1. pip uninstall InSilicoLVO -y"
-    echo "2. pip install --upgrade git+https://github.com/DIDSR/PedSilicoLVO.git"
+    echo "Error: Invalid version of generate function present. Please follow the steps below to reinstall InSilicoICH:"
+    echo "1. pip uninstall InSilicoICH -y"
+    echo "2. pip install --upgrade git+https://github.com/DIDSR/InSilicoICH.git"
 fi
